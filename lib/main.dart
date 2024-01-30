9502cddf19a55161595b0e26f25e32065471751e
Will Robbins <wrobbins@nd.edu>
2024-01-30 12:56:50 -0500

    Removed hard coded individual widget themes to instead use appainter theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,28 @@
 import 'package:atlas/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:json_theme/json_theme.dart';
 
-void main() {
-  runApp(const MainApp());
+import 'package:flutter/services.dart'; // For rootBundle
+import 'dart:convert'; // 
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final themeStr = await rootBundle.loadString('assets/appainter_theme.json');
+  final themeJson = jsonDecode(themeStr);
+  final theme = ThemeDecoder.decodeThemeData(themeJson)!;
+
+  runApp(MainApp(theme: theme));
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  final ThemeData theme;
 
+  const MainApp({Key? key, required this.theme}) : super(key: key);
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: MyHomePage()
-    );
+    return MaterialApp(home: const MyHomePage(), theme: theme);
   }
 }
