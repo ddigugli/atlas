f328f73671e79728634ade87d471268f703806c8
Will Robbins <wrobbins@nd.edu>
2024-01-29 23:09:24 -0500

    Added basic text input search bar functionality

--- lib/search_page.dart
+++ lib/search_page.dart
@@ -9,10 +9,60 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPageState extends State<SearchPage> {
+
+  // Constructor for search page
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Search Page'),
-    );
+    return Scaffold(
+      
+      // App bar that has title for search page
+      appBar: AppBar(
+        title: const Text(
+          'Find Friends, Workouts, and Groups',
+          style: TextStyle(
+            fontFamily: 'Consolas',
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+
+      // Creates body that contains search bar
+      body: SearchAnchor(
+        builder: (BuildContext context, SearchController controller) {
+          return SearchBar(
+            controller: controller, // set controller as a search controller for the search bar
+
+            // Open search bar view if search bar is clicked or text is changed
+            onTap: () {
+              controller.openView();
+            },
+            onChanged: (_) {
+              controller.openView();
+            },
+
+            // adds search icon to bar
+            leading: const Icon(Icons.search),
+          );
+        }, 
+
+        // create suggestions for search bar
+        suggestionsBuilder: (BuildContext context, SearchController controller) {
+          // TODO: Currently unfinished with numbered items as placeholders
+          return List<ListTile>.generate(5, (int index) {
+            final String item = 'item $index';
+
+            // generate list tiles from items and if it is clicked, close view and update controller with item
+            return ListTile(
+              title: Text(item),
+              onTap: () {
+                setState(() {
+                  controller.closeView(item);
+                });
+              },
+            );
+          });
+        }),
+      );
   }
 }
