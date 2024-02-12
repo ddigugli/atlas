c09cb6bae2f18f9106a0a6935360e98056724fa9
eamontracey <etracey@nd.edu>
2024-02-12 10:08:53 -0500

    configure firebase with the ios application

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,4 +1,6 @@
 import UIKit
+
+import FirebaseCore
 import Flutter
 
 @UIApplicationMain
@@ -7,6 +9,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    FirebaseApp.configure()
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
