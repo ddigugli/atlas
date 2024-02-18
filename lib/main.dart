4b969cff36e332eda0700cd4c439a03259804b5d
Declan Brown <dbrown39@nd.edu>
2024-02-18 15:37:57 -0500

    Fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 // Add useless comment!
 
-import 'package:atlas/home_page.dart';
+import 'package:atlas/screens/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:json_theme/json_theme.dart';
 
