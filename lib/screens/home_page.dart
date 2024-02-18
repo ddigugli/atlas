fd0c992041d4bcaac68c291243afd731ee1c1940
dbrown39nd <93266747+dbrown39nd@users.noreply.github.com>
2024-02-18 15:41:41 -0500

    Authentication (#18)
    * Added directories for organization in lib
    
    * Fixed
    
    ---------
    
    Co-authored-by: Yanni Vu <114590275+yannivu@users.noreply.github.com>

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,7 +1,7 @@
-import 'package:atlas/activity_dashboard.dart';
-import 'package:atlas/profile_page.dart';
-import 'package:atlas/search_page.dart';
-import 'package:atlas/workout_page.dart';
+import 'package:atlas/screens/activity_dashboard.dart';
+import 'package:atlas/screens/profile_page.dart';
+import 'package:atlas/screens/search_page.dart';
+import 'package:atlas/screens/workout_page.dart';
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatefulWidget {
@@ -12,12 +12,11 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  
   int _currentIndex = 0;
 
   final List<Widget> _children = const [
     ActivityDashboard(),
-    SearchPage(), 
+    SearchPage(),
     WorkoutPage(),
     ProfilePage()
   ];
@@ -25,21 +24,17 @@ class _MyHomePageState extends State<MyHomePage> {
   void onTabTapped(int index) {
     setState(() {
       _currentIndex = index;
-    }); 
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          'ATLAS',style: Theme.of(context).textTheme.headlineLarge
-        ),
+        title: Text('ATLAS', style: Theme.of(context).textTheme.headlineLarge),
         centerTitle: true,
       ),
-      body: Center(
-        child: _children[_currentIndex]
-      ),
+      body: Center(child: _children[_currentIndex]),
       bottomNavigationBar: BottomNavigationBar(
         onTap: onTabTapped,
         type: BottomNavigationBarType.fixed,
