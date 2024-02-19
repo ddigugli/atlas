a92c3eb1088d898e980fb8ad58f5601decbc2e56
Yanni Vu <114590275+yannivu@users.noreply.github.com>
2024-02-18 22:00:10 -0500

    Workout Cards and Settings Widget added to Profile Page (#19)

--- lib/screens/profile_page.dart
+++ lib/screens/profile_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'settings_page.dart';
 
 // Profile Page
 class ProfilePage extends StatefulWidget {
@@ -13,33 +14,47 @@ class _ProfilePageState extends State<ProfilePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Center(
-          child: Text('Profile', style: Theme.of(context).textTheme.titleMedium),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Spacer(),
+            const SizedBox(width: 40),
+            Text('Profile', style: Theme.of(context).textTheme.titleLarge),
+            const Spacer(), 
+            IconButton(
+              icon: const Icon(Icons.settings),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const SettingsPage()), 
+                );
+              },
+            ),
+          ],
         ),
       ),
-
       body: ListView(
-        children: const [
+        children: [
           Column(
-            crossAxisAlignment: CrossAxisAlignment.start, // Align children to the left
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Row(
+              const Row(
                 children: [
                   Padding(
-                    padding: EdgeInsets.only(top: 20.0, left: 20.0, right: 20.0), // Adjust the right padding
+                    padding: EdgeInsets.only(top: 20.0, left: 20.0, right: 20.0),
                     child: CircleAvatar(
                       radius: 50,
-                      backgroundImage: NetworkImage("https://image-cdn.essentiallysports.com/wp-content/uploads/arnold-schwarzenegger-volume-workout-1110x788.jpg"),
+                      backgroundImage: NetworkImage("https://image-cdn.essentiallysports.com/wp-content/uploads/arnold-schwarzenegger-volume-workout-1110x788.jpg"), // replace with user's profile picture
                     ),
                   ),
-                  SizedBox(width: 10.0), // Adjust the width of the SizedBox for additional space between CircleAvatar and Text
+                  SizedBox(width: 10.0),
                   Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       SizedBox(height: 15.0),
                       Text('Arnold Schwarzenegger', style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold)),
                       Text('@arnold', style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.bold)),
-                      SizedBox(height: 5.0), // Add a SizedBox to create space between Text widgets
+                      SizedBox(height: 5.0),
                       Row(
                         children: [
                           Column(
@@ -68,8 +83,18 @@ class _ProfilePageState extends State<ProfilePage> {
                   ),
                 ],
               ),
-              // Add more widgets to the Column if needed
-            ],
+              const SizedBox(height: 15.0),
+              ... List.generate(
+                10, // we would dynamically change this for each user depending on the number of workouts theyve posted
+                (index) {
+                  return const Card(
+                    child: ListTile(
+                    title: Text("Recent Workout Card"), // placeholder until we decide how each card should look
+                    )
+                  );
+                },
+              )
+            ]
           ),
         ],
       ),
