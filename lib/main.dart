4f3f1583c8810dece80499d7426113af1b1033bc
Yanni Vu <yvu@nd.edu>
2024-02-23 17:50:04 -0500

    Database Setup for Workouts and Exercises. Test workout showing on Activity Dashboard.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,3 @@
-// Add useless comment!
-
-
 import 'package:atlas/screens/wrapper.dart';
 import 'package:flutter/material.dart';
 import 'package:json_theme/json_theme.dart';
@@ -9,6 +6,8 @@ import 'package:flutter/services.dart'; // For rootBundle
 import 'package:provider/provider.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'firebase_options.dart';
+import 'package:atlas/services/database.dart';
+import 'package:uuid/uuid.dart';
 
 import 'dart:convert'; //
 
@@ -17,17 +16,59 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  // Load theme
   final themeStr = await rootBundle.loadString('assets/appainter_theme.json');
   final themeJson = jsonDecode(themeStr);
   final theme = ThemeDecoder.decodeThemeData(themeJson)!;
 
+  DatabaseService databaseService = DatabaseService();
+
+
+  // Lookup user ID by username
+  String userID = 'j8GXXkrN9Mc7BMCrkfoMuaVUt3o1';
+    // User ID found, proceed with adding workout
+    List<Map<String, dynamic>> exerciseList = [
+      {
+        'exerciseID': 'Pushups',
+        'sets': 3,
+        'reps': 10,
+        'restTime': '60s'
+      },
+      {
+        'exerciseID': 'Pushups',
+        'sets': 3,
+        'reps': 12,
+        'restTime': '60s'
+      },
+      // Add more exercises as needed
+    ];
+
+    // Generate a random workout ID
+    // var uuid = const Uuid();
+    // String workoutID = uuid.v4();
+
+    String workoutID = '20a1f112-52c3-40d2-a974-fd50222a82e3';
+
+    // Add workout
+    await databaseService.addWorkout(
+      workoutID,
+      'Test Workout',
+      userID,
+      'Test workout',
+      true,
+      exerciseList,
+    );
+
+  // Run the app
   runApp(MainApp(theme: theme));
 }
+
 
 class MainApp extends StatelessWidget {
   final ThemeData theme;
 
-  const MainApp({Key? key, required this.theme}) : super(key: key);
+  const MainApp({super.key, required this.theme});
 
   // This widget is the root of your application.
   @override
