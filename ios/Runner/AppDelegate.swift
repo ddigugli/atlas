fc7cda5b120fde6be9e13175f4f99ee7db43f15a
eamontracey <etracey@nd.edu>
2024-02-23 13:58:38 -0500

    resolve ios compilation issues

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-import FirebaseCore
+//import FirebaseCore
 import Flutter
 
 @UIApplicationMain
@@ -9,7 +9,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    FirebaseApp.configure()
+//    FirebaseApp.configure()
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
