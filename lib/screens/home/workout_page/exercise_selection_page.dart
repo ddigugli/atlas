d4cbb5ca205e36a3cf93b9b8e5481e29d59e24a2
Declan Brown <dbrown39@nd.edu>
2024-02-28 11:52:38 -0500

    Workout Builder solid progress. Still need to add firebase functionality to send it back to the backend

---
+++ lib/screens/home/workout_page/exercise_selection_page.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:atlas/models/exercise.dart'; // Ensure this path is correct
+
+class ExerciseSelectionPage extends StatefulWidget {
+  @override
+  _ExerciseSelectionPageState createState() => _ExerciseSelectionPageState();
+}
+
+class _ExerciseSelectionPageState extends State<ExerciseSelectionPage> {
+  List<Exercise> exercises = []; // Updated to store Exercise objects
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchExercises();
+  }
+
+  Future<void> _fetchExercises() async {
+    QuerySnapshot snapshot =
+        await FirebaseFirestore.instance.collection('exercises').get();
+    List<Exercise> fetchedExercises = snapshot.docs.map((doc) {
+      var data = doc.data() as Map<String, dynamic>;
+      return Exercise(
+          name:
+              data['exerciseName']); // Initializes Exercise objects with names
+    }).toList();
+
+    setState(() => exercises = fetchedExercises);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Select Exercise'),
+      ),
+      body: ListView.builder(
+        itemCount: exercises.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(exercises[index].name),
+            onTap: () {
+              // Directly pass the Exercise object back
+              Navigator.pop(context, exercises[index].name);
+            },
+          );
+        },
+      ),
+    );
+  }
+}
--- lib/screens/home/workout_page/workout_builder.dart
+++ lib/screens/home/workout_page/workout_builder.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:atlas/models/user.dart';
+import 'package:atlas/models/user.dart'; // Ensure this is the correct path for your User model
+import 'package:atlas/screens/home/workout_page/exercise_selection_page.dart'; // Verify the path
+import 'package:atlas/models/exercise.dart'; // Verify the path for your Exercise model
 
-// Workout Builder Page
 class WorkoutBuilder extends StatefulWidget {
   const WorkoutBuilder({Key? key}) : super(key: key);
 
@@ -11,7 +11,9 @@ class WorkoutBuilder extends StatefulWidget {
 }
 
 class _WorkoutBuilderState extends State<WorkoutBuilder> {
-  // Define your state variables here
+  String workoutName = '';
+  String workoutDescription = '';
+  List<Exercise> selectedExercises = [];
 
   @override
   Widget build(BuildContext context) {
@@ -25,16 +27,167 @@ class _WorkoutBuilderState extends State<WorkoutBuilder> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              // Your workout builder form fields and widgets here
-              Text('Build your workout plan'),
-              // Example: TextField(), DropdownButton(), etc.
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Workout Name',
+                  border: OutlineInputBorder(),
+                ),
+                onChanged: (value) => workoutName = value,
+              ),
+              SizedBox(height: 10),
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Workout Description',
+                  border: OutlineInputBorder(),
+                ),
+                onChanged: (value) => workoutDescription = value,
+              ),
+              SizedBox(height: 20),
+              ...selectedExercises.asMap().entries.map((entry) {
+                int idx = entry.key;
+                Exercise exercise = entry.value;
+                return ExerciseTile(
+                  exercise: exercise,
+                  onDelete: () =>
+                      setState(() => selectedExercises.removeAt(idx)),
+                  index: idx, // Pass the index to ExerciseTile
+                );
+              }).toList(),
+              Center(
+                child: ElevatedButton(
+                  onPressed: () => _navigateAndDisplaySelection(context),
+                  child: Text('Add Exercise'),
+                ),
+              ),
             ],
           ),
         ),
       ),
-      // Optionally, add a FloatingActionButton or ElevatedButton for actions like "Save Workout"
     );
   }
 
-  // Define any functions for handling state changes, form submissions, etc., here
+  Future<void> _navigateAndDisplaySelection(BuildContext context) async {
+    final String? exerciseName = await Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => ExerciseSelectionPage()),
+    );
+
+    if (exerciseName != null) {
+      _showAddExerciseDetailsDialog(exerciseName);
+    }
+  }
+
+  Future<void> _showAddExerciseDetailsDialog(String exerciseName) async {
+    TextEditingController setsController = TextEditingController();
+    TextEditingController repsController = TextEditingController();
+    TextEditingController weightController = TextEditingController();
+
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // User must tap a button to dismiss the dialog
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Add Details for $exerciseName'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                TextField(
+                  controller: setsController,
+                  decoration: InputDecoration(labelText: 'Sets'),
+                  keyboardType:
+                      TextInputType.text, // Sets can be a range, hence text
+                ),
+                TextField(
+                  controller: repsController,
+                  decoration: InputDecoration(labelText: 'Reps'),
+                  keyboardType:
+                      TextInputType.text, // Reps can be a range, hence text
+                ),
+                TextField(
+                  controller: weightController,
+                  decoration: InputDecoration(labelText: 'Weight (lbs)'),
+                  keyboardType: TextInputType.numberWithOptions(decimal: true),
+                ),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Cancel'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: Text('Add'),
+              onPressed: () {
+                // Add the new exercise with details to the list
+                setState(() {
+                  selectedExercises.add(Exercise(
+                    name: exerciseName,
+                    sets: setsController.text,
+                    reps: repsController.text,
+                    weight: weightController.text,
+                  ));
+                });
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
+
+class ExerciseTile extends StatelessWidget {
+  final Exercise exercise;
+  final VoidCallback onDelete;
+  final int index; // Added index to track the exercise position
+
+  const ExerciseTile({
+    Key? key,
+    required this.exercise,
+    required this.onDelete,
+    required this.index, // Require index in the constructor
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.symmetric(vertical: 5),
+      child: Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Text(
+                  '${index + 1}) ', // Display the exercise number
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+                Expanded(
+                  child: Text(
+                    exercise.name,
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ],
+            ),
+            Text(
+              'Sets: ${exercise.sets}, Reps: ${exercise.reps}, Weight: ${exercise.weight}lbs',
+            ),
+            Align(
+              alignment: Alignment.centerRight,
+              child: IconButton(
+                icon: Icon(Icons.delete, color: Colors.red),
+                onPressed: onDelete,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
