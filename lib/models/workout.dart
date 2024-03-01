4ed143801d8aaac3ba8c3516e40328bca4e71139
Will Robbins <wrobbins@nd.edu>
2024-03-01 13:15:28 -0500

    updated workout model

--- lib/models/workout.dart
+++ lib/models/workout.dart
@@ -1,33 +1,16 @@
-//Somehow define a model for what a workout looks like
-//This is AI generated code
-class Workout {
-  final String name;
-  final String description;
-  final String image;
-  final String video;
-  final String duration;
-  final String difficulty;
-  final String equipment;
-  final String type;
-  final String muscleGroup;
-  final String trainer;
-  final String rating;
-  final String reviews;
-  final String price;
+import 'package:atlas/models/exercise.dart';
 
-  const Workout({
-    required this.name,
-    required this.description,
-    required this.image,
-    required this.video,
-    required this.duration,
-    required this.difficulty,
-    required this.equipment,
-    required this.type,
-    required this.muscleGroup,
-    required this.trainer,
-    required this.rating,
-    required this.reviews,
-    required this.price,
+class Workout {
+  String createdBy;
+  String workoutName;
+  String description;
+  List<Exercise> exercises;
+  
+
+  Workout({
+    required this.createdBy,
+    required this.workoutName,
+    this.description = "",
+    required this.exercises
   });
 }
