7c43f3c68bc11c3e474de8dd2940b8ea9e8a0869
Yanni Vu <yvu@nd.edu>
2024-03-01 15:25:44 -0500

    Search for users working. Need to add functionality on click.

--- lib/screens/home/search_page.dart
+++ lib/screens/home/search_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 // Search Page
 class SearchPage extends StatefulWidget {
@@ -9,55 +10,85 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPageState extends State<SearchPage> {
-  // Constructor for search page
+  final TextEditingController _searchController = TextEditingController();
+  List<Map<String, dynamic>> users = [];
+  List<Map<String, dynamic>> filteredUsers = [];
+  bool showSuggestions = false;
+
+  @override
+  void initState() {
+    super.initState();
+    getUsers();
+  }
+
+  void getUsers() async {
+    // query users
+    QuerySnapshot querySnapshot =
+        await FirebaseFirestore.instance.collection('users').get();
+
+    setState(() {
+      users = querySnapshot.docs
+          .map((doc) => doc.data() as Map<String, dynamic>)
+          .toList();
+    });
+  }
+
+  void filterUsers(String query) {
+    setState(() {
+      filteredUsers = users
+          .where((user) => user['username']
+              .toLowerCase()
+              .contains(query.toLowerCase()))
+          .toList();
+      showSuggestions = true;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // App bar that has title for search page
       appBar: AppBar(
-        title: Text('Find Friends, Workouts, and Groups',
-            style: Theme.of(context).textTheme.titleMedium),
+        title: Text(
+          'Find Friends, Workouts, and Groups',
+          style: Theme.of(context).textTheme.titleMedium,
+        ),
       ),
 
-      // Creates body that contains search bar
-      body: SearchAnchor(
-          builder: (BuildContext context, SearchController controller) {
-        return SearchBar(
-          controller:
-              controller, // set controller as a search controller for the search bar
-
-          // Open search bar view if search bar is clicked or text is changed
-          onTap: () {
-            controller.openView();
-          },
-          onChanged: (_) {
-            controller.openView();
-          },
-
-          // adds search icon to bar
-          leading: const Icon(Icons.search),
-          hintText: 'Search...',
-        );
-      },
-
-          // create suggestions for search bar
-          suggestionsBuilder:
-              (BuildContext context, SearchController controller) {
-        // TODO: Currently unfinished with numbered items as placeholders
-        return List<ListTile>.generate(5, (int index) {
-          final String item = 'item $index';
-
-          // generate list tiles from items and if it is clicked, close view and update controller with item
-          return ListTile(
-            title: Text(item),
-            onTap: () {
-              setState(() {
-                controller.closeView(item);
-              });
-            },
-          );
-        });
-      }),
+      // contains search bar and filtered user list
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: _searchController,
+              onChanged: (value) {
+                filterUsers(value);
+              },
+              onTap: () {
+                setState(() {
+                  showSuggestions = false;
+                });
+              },
+              decoration: const InputDecoration(
+                prefixIcon: Icon(Icons.search),
+                hintText: 'Search...',
+              ),
+            ),
+          ),
+          if (showSuggestions)
+            Expanded(
+              child: ListView.builder(
+                itemCount: filteredUsers.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(filteredUsers[index]['username']),
+                    // Add functionality for when you tap on a result
+                  );
+                },
+              ),
+            ),
+        ],
+      ),
     );
   }
 }
